8df7c72a1f43431cabcdc9bdfbdd8e4c8e0543df
sofia <sofia@sofia-virtual-machine.(none)>
2013-03-01 13:33:06 -0800

    verification login and pass

--- app/controllers/api_session_controller.rb
+++ app/controllers/api_session_controller.rb
@@ -2,10 +2,17 @@ class ApiSessionController < ApplicationController
 
   def create
     user = User.find_by_login(params[:login])
-    
-    respond_to do |format|
-      format.html # create.html.erb
-      format.json { render json: user.authentication_token }
+    if user == nil || user.valid_password?(params[:password]) == false
+      respond_to do |format|
+        format.html # create.html.erb
+        format.json { render json: "error" }
+      end
+      else
+      respond_to do |format|
+        format.html # create.html.erb
+        format.json { render json: user.authentication_token }
+      end
     end
   end
 end
+  
