239c227b2e3537d4833aa27004869aa5fcd522e0
sofia <sofia@sofia-virtual-machine.(none)>
2013-03-10 19:51:02 -0700

    article edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -92,23 +92,33 @@ class ArticlesController < ApplicationController
   # PUT /articles/1
   # PUT /articles/1.json
   def update
-    @article = Article.find(params[:id])
-
+    @user = User.find_by_authentication_token(params[:auth_token])
+    @newArticle = Article.new.from_json(params[:article])
+    @oldArticle = Article.find(@newArticle.id)
+    @author = Author.find_by_user_id_and_article_id(@user.id, @newArticle.id)
+    if @author != nil
+    @oldArticle.content = @newArticle.content
+    @oldArticle.category = @newArticle.category
+    @oldArticle.title = @newArticle.title
     respond_to do |format|
-      if @article.update_attributes(params[:article])
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+      if @oldArticle.save
+        format.html { redirect_to @oldArticle, notice: 'Article was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+        format.json { render json: @oldArticle.errors, status: :unprocessable_entity }
       end
+    end
+    else
+      respond_to do |format|
+        format.html { render json: "error" }
+        format.json { render json: "error" }
+    end
     end
   end
 
   def pdestroy
     @user = User.find_by_authentication_token(params[:auth_token])
-    puts "here"
-    puts @user.id
     @author = Author.find_by_user_id_and_article_id(@user.id, params[:id])
     if (@author != nil)
       @article = Article.find(params[:id])
